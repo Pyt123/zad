562c2a23204dd4acbe15828ab0a4855e1082d065
DESKTOP-NH1GAIE\Dawid <dawidantczak1997@wp.pl>
2018-05-20 04:04:58 +0200

    no

--- content.py
+++ content.py
@@ -25,81 +25,11 @@ class NeuralNet(nn.Module):
         return self.fc2(x)
 
 
-def train():
-    COUNT = 27500
-    EPOCHS = 100000
-    START_MOMENTUM = 0.5
-    MOMENTUM = START_MOMENTUM
-    START_LR = 0.5
-    LR = START_LR
-    DIVIDER = 1.2
-    EPOCHS_TO_CHANGE = 50
-    NEXT_TO_CHANGE = EPOCHS_TO_CHANGE
-    BATCH_SIZE = 500
-    START_TO_ADAM = 20000
-
-    # Load data
-    (x_train, y_train) = pkl.load(open('train.pkl', mode='rb'))
-    (x_train, y_train) = (x_train[:COUNT], y_train[:COUNT])
-
-    # Create model
-    model = NeuralNet().cuda()
-    # Load model
-    #model = torch.load('mytraining.pth')
-
-    # Some stuff
-    #optimizer = optim.RMSprop(model.parameters())
-    optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
-    criterion = nn.CrossEntropyLoss()
-
-    model.train()
-
-    # Convert numpy arrays to torch variables
-    inputs = torch.autograd.Variable(torch.from_numpy(x_train).type(torch.cuda.FloatTensor), requires_grad=True)
-    targets = torch.autograd.Variable(torch.from_numpy(y_train).type(torch.cuda.LongTensor), requires_grad=False)
-    targets = targets.squeeze(1)
-
-    for epoch in range(EPOCHS):
-        if epoch == NEXT_TO_CHANGE:
-            MOMENTUM /= DIVIDER
-            LR /= DIVIDER
-            if epoch < START_TO_ADAM:
-                optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
-            NEXT_TO_CHANGE += EPOCHS_TO_CHANGE
-            torch.save(model, 'mytraining.pth')
-        if epoch == START_TO_ADAM:
-            optimizer = optim.Adam(model.parameters(), lr=1e-3, eps=1e-3)
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if epoch % 50 == 0:
-            print('Epoch [{}/{}],\tLoss: {:.24f}'.format(epoch, EPOCHS, loss.data[0]))
-
-    torch.save(model, 'mytraining.pth')
-    return model
 
 
 
-'''def predict(x):
-    x_train, y_train = pkl.load(open('train.pkl', mode='rb'))
-    x_train, y_train = x_train[:6], y_train[:6]
-    return y_train[np.argmin(
-        2.5 * x.astype(np.uint16) @ ~x_train.astype(np.bool).transpose() + ~x.astype(np.bool) @ x_train.astype(
-            np.uint16).transpose(), axis=1)]
 
-
-x_train, y_train = pkl.load(open('train.pkl', mode='rb'))
-x_train = x_train[6:10]
-pred = predict(x_train)
-print(type(pred))
-print(pred.shape)'''
-
-def compress_training_set(x_train):
+'''def compress_training_set(x_train):
     new_x_train_array = []
     for x in x_train:
         max = len(x)
@@ -111,4 +41,4 @@ def compress_training_set(x_train):
                 i += 56
         new_x_train_array.append(np.array(new_x_array))
 
-    return np.array(new_x_train_array)
+    return np.array(new_x_train_array)'''
--- predict.py
+++ predict.py
@@ -10,8 +10,12 @@ import content
 import time
 import torch
 import pickle as pkl
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
 import numpy as np
 
+GLOBAL_COUNTER = 1
 
 def save_model_as_numpy(model):
     i = 1
@@ -37,7 +41,8 @@ def relu(x):
     return x * (x > 0)
 
 
-def get_predicted(x):
+def get_predicted(x, model):
+    save_model_as_numpy(model)
     (w1, _, w2, _) = load_model_from_file()
     w1 = np.transpose(w1).copy(order='C')
     w2 = np.transpose(w2).copy(order='C')
@@ -56,68 +61,87 @@ def get_predicted(x):
     return output_vector.reshape((len(output_vector), 1))
 
 
-def predict(x):
-    """
-    Funkcja pobiera macierz przykladow zapisanych w macierzy X o wymiarach NxD i zwraca wektor y o wymiarach Nx1,
-    gdzie kazdy element jest z zakresu {0, ..., 35} i oznacza znak rozpoznany na danym przykladzie.
-    :param x: macierz o wymiarach NxD
-    :return: wektor o wymiarach Nx1
-    """
+def train(x_train, y_train, num_of_try, learning_rate):
+    EPOCHS = 50000
+    #START_MOMENTUM = 1
+    #MOMENTUM = START_MOMENTUM
+    #START_LR = 1
+    #LR = START_LR
+    #DIVIDER_MOM = 1.03
+    #DIVIDER_LR = 1.05
+    EPOCHS_TO_CHANGE = 1000
+    NEXT_TO_CHANGE = EPOCHS_TO_CHANGE
+    LAST_BEST = 0
+    BEST_EPOCH = 0
 
-    # -------------------------------------------------------------
-    print('start')
-    start_time = time.time()
-    (w1, _, w2, _) = load_model_from_file()
-    w1 = np.transpose(w1)
-    w2 = np.transpose(w2)
-    w1 = w1.copy(order='C')
-    w2 = w2.copy(order='C')
-    output_array = []
-    length = len(x)
-    #output = np.empty((length,), order='C')
-    layer1 = np.empty((900,), order='C')
-    layer2 = np.empty((36,), order='C')
-    #layer1 = np.asarray(layer1)
-    #layer2 = np.asarray(layer2)
-    for i in range(0, length):
-        np.matmul(x[i], w1, out=layer1)
-        layer1 = relu(layer1)
-        np.matmul(layer1, w2, out=layer2)
-        layer2 = relu(layer2)
-        #output.setfield(np.int(arg), np.int, i)
-        output_array.append(layer2.argmax())
+    # Create model
+    model = content.NeuralNet().cuda()
+    # Load model
+    #model = torch.load('mytraining.pth')
 
-    output = np.array(output_array)
-    print('time needed: ' + str(time.time() - start_time) + ' s')
-    #return output
-    # -------------------------------------------------------------
-    np.save('predicted_vals', output)
+    # Some stuff
+    #optimizer = optim.RMSprop(model.parameters())
+    #optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, eps=1e-3)
+    criterion = nn.CrossEntropyLoss()
+    model.train()
 
+    # Convert numpy arrays to torch variables
+    inputs = torch.autograd.Variable(torch.from_numpy(x_train).type(torch.cuda.FloatTensor), requires_grad=True)
+    targets = torch.autograd.Variable(torch.from_numpy(y_train).type(torch.cuda.LongTensor), requires_grad=False)
+    targets = targets.squeeze(1)
+
+    for epoch in range(EPOCHS):
+        if epoch == NEXT_TO_CHANGE:
+            #MOMENTUM /= DIVIDER_MOM
+            #LR /= DIVIDER_LR
+            #optimizer = optim.SGD(model.parameters(), lr=LR, momentum=MOMENTUM)
+            NEXT_TO_CHANGE += EPOCHS_TO_CHANGE
+            ratio = test(model, y_val)
+            if ratio > LAST_BEST:
+                torch.save(model, 'bestmodel' + str(num_of_try) + '.pth')
+                LAST_BEST = ratio
+                BEST_EPOCH = epoch
+                print('-----Best epoch: ' + str(BEST_EPOCH) + '\tBest ratio: ' + str(LAST_BEST) + '-----')
+
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if epoch % 500 == 0:
+            print('Epoch [{}/{}],\tLoss: {:.24f}'.format(epoch, EPOCHS, loss.data[0]))
+
+    torch.save(model, 'endingmodel' + str(num_of_try) + '.pth')
+    print('END-----Best epoch: ' + str(BEST_EPOCH) + '\tBest ratio: ' + str(LAST_BEST) + '-----')
+    return model
+
+
+def test(model, y_val):
+    pred = get_predicted(x_val, model)
     good = 0
     for i in range(0, 2634):
-        if output[i] == y_train[i]:
+        if pred[i] == y_val[i]:
             good += 1
-    print("good: " + str(good))
-    print("ratio: " + str(good/2634.0 * 100))
-    return output
+    ratio = (good / 2634.0) * 100
+    print("ratio: " + str(ratio))
+    return ratio
 
 
-content.train()
+(x, y) = pkl.load(open('train.pkl', mode='rb'))
+(x_train, y_train) = (x[:27500], y[:27500])
+(x_val, y_val) = (x[27500:], y[27500:])
 
-(x_train, y_train) = pkl.load(open('train.pkl', mode='rb'))
-(x_train, y_train) = (x_train[27500:], y_train[27500:])
-
-model = torch.load('mytraining.pth', 'cpu')
-save_model_as_numpy(model)
-print('saved as numpy')
-
-pred = get_predicted(x_train)
-
-good = 0
-for i in range(0, 2634):
-    if pred[i] == y_train[i]:
-        good += 1
-print("good: " + str(good))
-print("ratio: " + str(good / 2634.0 * 100))
+learning_rates = [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005]
+for i in range(len(learning_rates)):
+    print('Testing learning rate = ' + str(learning_rates[i]))
+    train(x_train, y_train, i, learning_rates[i])
+    print('\n')
 
 exit(0)
+
+#save_model_as_numpy(model)
+#print('saved as numpy')
